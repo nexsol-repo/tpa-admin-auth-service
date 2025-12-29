d1bcf3f60e05db0eeb306f2628ed8fae780ce3c5
devback90 <devback90.nexsol@gmail.com>
2025-12-29 17:59:01 +0900

    fix: ci/cd

--- core/core-api/src/main/java/com/nexsol/tpa/CoreApiApplication.java
+++ core/core-api/src/main/java/com/nexsol/tpa/CoreApiApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
 @ConfigurationPropertiesScan(basePackages = "com.nexsol.tpa") // 설정값(Properties) 스캔 범위 확장
-@ComponentScan(basePackages = "com.nexsol.tpa")
 public class CoreApiApplication {
 
     public static void main(String[] args) {
