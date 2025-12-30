8332aa2e431470abf9837a8ce1feb33aa6bee8fa
devback90 <devback90.nexsol@gmail.com>
2025-12-30 14:17:39 +0900

    fix: ci/cd

--- core/core-api/src/main/java/com/nexsol/tpa/CoreApiApplication.java
+++ core/core-api/src/main/java/com/nexsol/tpa/CoreApiApplication.java
@@ -5,15 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {
-        "com.nexsol.tpa.core",
-        "com.nexsol.tpa.storage",
-        "com.nexsol.tpa.support.token"
-})
-@ConfigurationPropertiesScan(basePackages = {
-        "com.nexsol.tpa.core",
-        "com.nexsol.tpa.storage"
-})
+@SpringBootApplication(scanBasePackages = "com.nexsol.tpa")
+@ConfigurationPropertiesScan(basePackages = "com.nexsol.tpa")
 public class CoreApiApplication {
 
     public static void main(String[] args) {
