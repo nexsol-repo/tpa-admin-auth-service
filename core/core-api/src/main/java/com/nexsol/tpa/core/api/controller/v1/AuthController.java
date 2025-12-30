deca53a7e5cf23aac771f89644fb8f7c2ffead91
devback90 <devback90.nexsol@gmail.com>
2025-12-30 09:15:25 +0900

    fix: ci/cd

--- core/core-api/src/main/java/com/nexsol/tpa/core/api/controller/v1/AuthController.java
+++ core/core-api/src/main/java/com/nexsol/tpa/core/api/controller/v1/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("v1/admin/auth")
+@RequestMapping("/v1/admin/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
