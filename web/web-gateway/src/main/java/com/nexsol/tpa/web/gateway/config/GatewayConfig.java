aa1e49bbcfed7a8443af9b617c927ebd9a5eb1cd
devback90 <devback90.nexsol@gmail.com>
2025-12-31 16:00:16 +0900

    fix: 게이트웨이

--- web/web-gateway/src/main/java/com/nexsol/tpa/web/gateway/config/GatewayConfig.java
+++ web/web-gateway/src/main/java/com/nexsol/tpa/web/gateway/config/GatewayConfig.java
@@ -13,23 +13,25 @@ public class GatewayConfig {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder, ScopeCheckGatewayFilterFactory scopeCheckFactory) {
         // 환경변수 없으면 디폴트값 사용 (안전장치)
         String authUrl = System.getenv().getOrDefault("AUTH_SERVICE_URL", "http://auth:8081");
-        String pungsuUrl = System.getenv().getOrDefault("PUNGSU_SERVICE_URL", "http://host.docker.internal:80/upstream/pungsu");
-        String memoUrl = System.getenv().getOrDefault("MEMO_SERVICE_URL","http://host.docker.internal:80/upstream/memo");
+        String pungsuUrl = System.getenv().getOrDefault("PUNGSU_SERVICE_URL", "http://host.docker.internal:80/upstream/pungsu/");
+        String memoUrl = System.getenv().getOrDefault("MEMO_SERVICE_URL","http://host.docker.internal:80/upstream/memo/");
 
         return builder.routes()
                 .route("auth-service", r -> r.path("/v1/admin/auth/**").uri(authUrl))
 
                 .route("pungsu-service", r -> r.path("/v1/admin/pungsu/**")
                         .filters(f -> f
-                                .stripPrefix(3) // "/v1/admin/pungsu" 제거하여 "/upstream/pungsu/" 뒤에 붙임
+                                // .stripPrefix(3)  <-- 이 줄을 삭제하거나 주석 처리하세요.
                                 .filter(scopeCheckFactory.apply(c -> c.setRequiredScope("PUNGSU"))))
                         .uri(pungsuUrl))
 
                 .route("memo-service", r -> r.path("/v1/admin/memo/**")
                         .filters(f -> f
-                                .stripPrefix(3) // "/v1/admin/memo" 제거하여 "/upstream/memo/" 뒤에 붙임
+                                // .stripPrefix(3)  <-- 이 줄을 삭제하거나 주석 처리하세요.
                                 .filter(scopeCheckFactory.apply(c -> c.setRequiredScope("MEMO"))))
                         .uri(memoUrl))
+
+                .route("pungsu-docs", r -> r.path("/v1/admin/pungsu/docs/**").uri(pungsuUrl))
                 .build();
     }
 }
